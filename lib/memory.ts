0adc3cdca894958e07f0fe8d49a727e2e4493cba
Lakshay <lakshaysh969@gmail.com>
2024-01-20 14:05:18 +0530

    fix22

--- lib/memory.ts
+++ lib/memory.ts
@@ -1,6 +1,6 @@
 import { Redis } from "@upstash/redis";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { PineconeClient } from '@pinecone-database/pinecone';
+import {PineconeClient} from '@pinecone-database/pinecone';
 
 import { PineconeStore } from "langchain/vectorstores/pinecone";
 
